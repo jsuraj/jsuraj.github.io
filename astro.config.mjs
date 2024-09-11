6ab545731915d62e6f19de43b79be14ca9565be1
Suraj Jagtap <jsurajbaramati@gmail.com>
2024-09-11 14:14:31 +0530

    remove base astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-const isProd = import.meta.env.PROD;
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jsuraj.github.io',
-  ...(isProd ? { base: 'jsuraj.github.io' } : {}),
   integrations: [tailwind()],
 });
