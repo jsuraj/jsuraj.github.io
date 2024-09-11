48efaafeccf81ceb6b1ad6a45d419ee09d23e3c1
Suraj Jagtap <jsurajbaramati@gmail.com>
2024-09-11 13:12:28 +0530

    changes to deploy using github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jsuraj.github.io',
+  base: 'jsuraj.github.io',
   integrations: [tailwind()]
 });
