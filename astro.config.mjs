0cef4d141ccc4c0525287bb8a8da75b8c9c6dd3c
Suraj Jagtap <jsurajbaramati@gmail.com>
2024-09-12 09:28:01 +0530

    added prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jsuraj.github.io',
+  site: "https://jsuraj.github.io",
   integrations: [tailwind()],
 });
