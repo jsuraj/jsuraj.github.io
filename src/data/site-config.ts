44c529559f383daf0b161b1ac4540a9efb49560e
Suraj Jagtap <jsurajbaramati@gmail.com>
2024-09-13 14:00:24 +0530

    added basic layout, header, footer, nav, theme

---
+++ src/data/site-config.ts
@@ -0,0 +1,60 @@
+export type Image = {
+  src: string;
+  alt?: string;
+  caption?: string;
+};
+
+export type Link = {
+  text: string;
+  href: string;
+};
+
+export type Hero = {
+  title?: string;
+  text?: string;
+  image?: Image;
+  actions?: Link[];
+};
+
+export type Subscribe = {
+  title?: string;
+  text?: string;
+  formUrl: string;
+};
+
+export type SiteConfig = {
+  logo?: Image;
+  title: string;
+  subtitle?: string;
+  description: string;
+  image?: Image;
+  headerNavLinks?: Link[];
+  footerNavLinks?: Link[];
+  socialLinks?: Link[];
+  hero?: Hero;
+  subscribe?: Subscribe;
+  postsPerPage?: number;
+  projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+  title: "Suraj Jagtap",
+  subtitle: "",
+  description: "",
+  image: {
+    src: "",
+    alt: "",
+  },
+  headerNavLinks: [
+    {
+      text: "Home",
+      href: "/",
+    },
+    {
+      text: "Projects",
+      href: "/projects",
+    },
+  ],
+};
+
+export default siteConfig;
